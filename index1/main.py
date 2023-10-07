7199ef2578f6a44d92692b60eb36bf0185a23d39
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-10-07 20:00:07 +0200

    cleanup

--- index1/main.py
+++ index1/main.py
@@ -1,36 +1,27 @@
-from llama_index.callbacks import CallbackManager, LlamaDebugHandler, CBEventType
-
-import frontmatter
-from datetime import date
-from llama_index import SimpleDirectoryReader
-from llama_index.node_parser import SimpleNodeParser
-from llama_index.readers.base import BaseReader
-from llama_index.prompts import PromptTemplate
-import json
-from pathlib import Path
-from llama_index.node_parser.file.markdown import MarkdownNodeParser
-from MarkdownReader import MarkdownReader, dict_string_values
-from llama_index.node_parser import SentenceWindowNodeParser
+import sys
 import re
+import json
+import logging
+import frontmatter
 import chromadb
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+
+from datetime import date
+from pathlib import Path
+
+from llama_index import SimpleDirectoryReader, VectorStoreIndex
 from llama_index.vector_stores import ChromaVectorStore
 from llama_index.storage.storage_context import StorageContext
-from llama_index.embeddings import HuggingFaceEmbedding
-from llama_index.llms import HuggingFaceLLM
+from llama_index import ServiceContext
+from llama_index.node_parser.file.markdown import MarkdownNodeParser
 
-from llama_index.node_parser.extractors import (
-    MetadataExtractor,
-    TitleExtractor,
-    QuestionsAnsweredExtractor,
-)
+from MarkdownReader import MarkdownReader, dict_string_values
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 
 def get_file_metadata(filename):
     post = frontmatter.load(filename)
-    # print(post)
-    # if not post.metadata:
-    #   post.metadata["title"] = "xxx"
     return {"filename": filename, **post.metadata}
 
 
@@ -55,7 +46,6 @@ sre_files_mapping = {
 
 
 def get_sre_metadata(filename):
-    # print(filename)
     metadata = get_file_metadata(filename)
     for path in sre_files_mapping.keys():
         if bool(re.match(path, filename)):
@@ -66,17 +56,15 @@ def get_sre_metadata(filename):
     return metadata
 
 
-incubators = json.loads(Path("./index1/incubators.json").read_text())
-
-print(incubators.get("sgmas", {}).get("title"))
-
-
 def get_last_phase(metadata):
     if metadata.get("phases"):
         metadata.get("phases").sort(key=lambda x: x.get("start", date(1970, 1, 1)))
         latest = metadata.get("phases")[-1]
         return latest.get("name")
     return None
+
+
+incubators = json.loads(Path("./index1/incubators.json").read_text())
 
 
 def get_se_metadata(filename):
@@ -88,11 +76,11 @@ def get_se_metadata(filename):
         "phase": get_last_phase(metadata)
         # "sponsors": metadata.get("sponsors"),
     }
-    # print(filename, result)
     return result
 
 
 def get_documents(source):
+    """return Document for given source(path, file_metadata)"""
     reader = SimpleDirectoryReader(
         input_dir=source.get("path"),
         required_exts=[".md"],
@@ -100,10 +88,8 @@ def get_documents(source):
         file_extractor={".md": MarkdownReader(source.get("include_metas", []))},
         file_metadata=source.get("file_metadata"),
     )
-    # use MarkdownReader and split top-level paragraphs into documents
+    # use MarkdownReader
     docs = reader.load_data()
-    # for doc in docs:
-    #     doc.metadata = source.get("file_metadata")(doc.filename)
     return docs
 
 
@@ -119,9 +105,9 @@ sources = [
     # },
     {
         "id": "startups-beta-gouv",
-        "topics": [
-            "Questions concernant les startups beta.gouv",
-        ],
+        # "topics": [
+        #     "Questions concernant les startups beta.gouv",
+        # ],
         "path": "./content/startups",
         "file_metadata": get_se_metadata,
         "include_metas": ["contact"],
@@ -140,10 +126,10 @@ sources = [
     },
     {
         "id": "support-techique-sre-socialgouv",
-        "topics": [
-            "Questions techniques sur le fonctionnent de l'hebergement",
-            "Questions sur kubernetes et la plateforme de la fabrique",
-        ],
+        # "topics": [
+        #     "Questions techniques sur le fonctionnent de l'hebergement",
+        #     "Questions sur kubernetes et la plateforme de la fabrique",
+        # ],
         "path": "./content/support-sre",
         "file_metadata": get_sre_metadata,
         "examples": [
@@ -155,111 +141,18 @@ sources = [
 ]
 
 
-# metadata_extractor = MetadataExtractor(
-#     extractors=[
-#         # TitleExtractor(nodes=5),
-#         # QuestionsAnsweredExtractor(questions=3),
-#     ],
-# )
-
-# text_splitter = SentenceWindowNodeParser(
-#     window_size=3,
-# )
-
-
-# node_parser = SimpleNodeParser.from_defaults(chunk_size=1024, chunk_overlap=20)
-
-node_parser = MarkdownNodeParser.from_defaults()
-
-# node_parser = SentenceWindowNodeParser.from_defaults(
-#     # text_splitter=text_splitter,
-#     window_size=0,
-#     metadata_extractor=metadata_extractor,
-# )
-# assume documents are defined -> extract nodes
-
-
-chroma_client = chromadb.PersistentClient(path="./chroma_db")
-# chroma_client = chromadb.EphemeralClient()
-
-
-# define embedding function
-# embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-
-# llm = OpenAI(model="text-davinci-003", temperature=0, max_tokens=256)
-
-# system_prompt = """<|SYSTEM|># StableLM Tuned (Alpha version)
-# - StableLM is a helpful and harmless open-source AI language model developed by StabilityAI.
-# - StableLM is excited to be able to help the user, but will refuse to do anything that could be considered harmful to the user.
-# - StableLM is more than just an information source, StableLM is also able to write poetry, short stories, and make jokes.
-# - StableLM will refuse to participate in anything that could harm a human.
-# """
-
-
-# query_wrapper_prompt = PromptTemplate("<|USER|>{query_str}<|ASSISTANT|>")
-
-
-# llm = HuggingFaceLLM(
-#     context_window=2048,
-#     max_new_tokens=256,
-#     generate_kwargs={"temperature": 0.25, "do_sample": False},
-#     # system_prompt=system_prompt,
-#     query_wrapper_prompt=query_wrapper_prompt,
-#     # tokenizer_name="StabilityAI/stablelm-tuned-alpha-3b",
-#     # model_name="StabilityAI/stablelm-tuned-alpha-3b",
-#     tokenizer_name="Writer/camel-5b-hf",
-#     model_name="Writer/camel-5b-hf",
-#     device_map="auto",
-#     # stopping_ids=[50278, 50279, 50277, 1, 0],
-#     # tokenizer_kwargs={"max_length": 2048},
-#     # uncomment this if using CUDA to reduce memory usage
-#     # model_kwargs={"torch_dtype": torch.float16}
-# )
-
-
-llama_debug = LlamaDebugHandler(print_trace_on_end=True)
-callback_manager = CallbackManager([llama_debug])
-
-
-service_context = ServiceContext.from_defaults(
-    # chunk_size=512,
-    # embed_model=embed_model,
-    callback_manager=callback_manager,
-    node_parser=node_parser,
-    # llm=llm,
-)
-
-
-for source in sources:
+def index_source(chroma_client, source):
+    """index given source in chromadb"""
     docs = get_documents(source)
     try:
         chroma_collection = chroma_client.get_collection(source.get("id"))
-        print("==> Collection {} already exist\n\n".format(source.get("id")))
+        logging.info("==> Collection {} already exist\n\n".format(source.get("id")))
     except ValueError:
         nodes = node_parser.get_nodes_from_documents(docs)
-        print("NODE metadata", nodes[-1].metadata)
         chroma_collection = chroma_client.create_collection(source.get("id"))
         vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
-        # for doc in docs:
-        #    print(doc.metadata)
-
-        # for node in nodes:
-        # print(node.metadata)
-        #     nodes = node_parser.get_nodes_from_documents([doc], show_progress=True)
-        #     print(doc.metadata.get("filename"), len(nodes))
-        # for node in nodes:
-        #     print(node.metadata)
-        #     print(node.relationships)
-        # print(doc.metadata)
-        # print(dir(doc))
-        # print(doc.json())
-        # print(len(nodes))
-        # for node in nodes:
-        # print("\n")
-        # print(node)
-        # print(node.metadata)
-        print(
+        logging.info(
             "index {} documents and {} nodes in {}".format(
                 len(docs), len(nodes), source.get("id")
             )
@@ -267,15 +160,27 @@ for source in sources:
         index = VectorStoreIndex.from_documents(
             docs, storage_context=storage_context, service_context=service_context
         )
-        print(f"==> Loaded {len(docs)} docs\n\n")
-        # print(nodes[-1])
-
-        # print("DOC metadata", docs[-1].metadata)
+        logging.info(f"==> Loaded {len(docs)} docs\n\n")
         print("\n\n")
     finally:
         vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
         index = VectorStoreIndex.from_vector_store(vector_store)
+    return index
 
+
+node_parser = MarkdownNodeParser.from_defaults()
+
+chroma_client = chromadb.PersistentClient(path="./chroma_db")
+
+service_context = ServiceContext.from_defaults(
+    # chunk_size=512,
+    # embed_model=embed_model,
+    node_parser=node_parser,
+    # llm=llm,
+)
+
+for source in sources:
+    index = index_source(chroma_client, source)
     query_engine = index.as_query_engine()
     for query in source.get("examples"):
         response = query_engine.query(query)
@@ -283,3 +188,4 @@ for source in sources:
         print(str(response))
         print((response.get_formatted_sources()))
         # print((response.source_nodes))
+        print("\n-------------\n")
